9789e20626ef72a078a91b0fe01e7c94b67afbfd
dpavlenko <pavlenkod.v.2@gmail.com>
2025-06-28 00:21:18 +0300

    initial

--- frontend/generated/jar-resources/material-includes.ts
+++
@@ -1,8 +0,0 @@
-import {colorLight, typography} from '@vaadin/vaadin-material-styles';
-
-const tpl = document.createElement('template');
-tpl.innerHTML = `<style>
-  ${colorLight.cssText}
-  ${typography.cssText}
-</style>`;
-document.head.appendChild(tpl.content);
